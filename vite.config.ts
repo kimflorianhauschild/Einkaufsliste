9f9529e562cc727237fe0ccf1bac4766a339233f
Kim Hauschild <kim.hauschild@dataciders.com>
2026-09-02 10:48:06 +0200

    Deploy Einkaufsliste to GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
+  base: "/Einkaufsliste/",
   plugins: [
     react(),
     VitePWA({
@@ -16,7 +17,7 @@ export default defineConfig({
         background_color: "#f8fafc",
         display: "standalone",
         orientation: "portrait",
-        start_url: "/",
+        start_url: "./",
         icons: [
           {
             src: "favicon.svg",
